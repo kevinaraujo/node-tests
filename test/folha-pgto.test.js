248a570045cd8936d102003d01c71e531a4ff68b
Kevin Araújo <kfa_34@hotmail.com>
2024-01-18 15:35:59 -0300

    doc: Changed gitignore. feat: Added describe method on folha-pgto file.

--- test/folha-pgto.test.js
+++ test/folha-pgto.test.js
@@ -1,15 +1,17 @@
 import { somaHorasExtras, calculaDescontos } from "..";
 
-test("somaHorasExtras should return the sum of over time.", () => {
-    const expected = 100;
-    const response = somaHorasExtras(50, 50);
-
-    expect(response).toBe(expected);
-});
-
-test("Should discount the salary", () => {
-    const expected = 2000;
-    const response = calculaDescontos(2500, 500);
-
-    expect(response).toBe(expected);
+describe('Tests to calculate salary.', () => {
+    it("The method called somaHorasExtras should return the sum of over time.", () => {
+        const expected = 100;
+        const response = somaHorasExtras(50, 50);
+        
+        expect(response).toBe(expected);
+    });
+    
+    it("Should discount the salary", () => {
+        const expected = 2000;
+        const response = calculaDescontos(2500, 500);
+        
+        expect(response).toBe(expected);
+    });
 });
