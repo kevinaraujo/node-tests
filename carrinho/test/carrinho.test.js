c58c589c29264a689f0a733c37c854b560100094
Kevin Araújo <kfa_34@hotmail.com>
2024-01-22 16:57:42 -0300

    test: Added new tests on carrinho test file to coverage 100% the code. Added folder 'coverage' on gitignore.

--- carrinho/test/carrinho.test.js
+++ carrinho/test/carrinho.test.js
@@ -1,46 +1,66 @@
-
-import Carrinho from "../carrinho";
-import Item from "../item.js";
+import Carrinho from '../carrinho';
+import Item from '../item.js';
 
 describe('Test the carrinho methods.', () => {
-    it('Should have initialize empty', () => {
-        const carrinho = new Carrinho();
+  it('Should have initialize empty', () => {
+    const carrinho = new Carrinho();
 
-        expect(carrinho.itens).toEqual([]);
-        expect(carrinho.subtotal).toBeNull();
-        expect(carrinho.total).toBeNull();
+    expect(carrinho.itens).toEqual([]);
+    expect(carrinho.subtotal).toBeNull();
+    expect(carrinho.total).toBeNull();
+  });
+
+  it('Should have items.', () => {
+    const item1 = new Item('Beterraba', 2.5, 10);
+    const item2 = new Item('Beterraba', 3, 5);
+
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item1);
+    carrinho.adiciona(item2);
+
+    expect(typeof carrinho.itens).toBe('object');
+    expect(carrinho.itens[0]).toEqual(item1);
+    expect(carrinho.itens[1]).toEqual(item2);
+
+    expect(carrinho.itens).toContain(item1);
+    expect(carrinho.itens).toContain(item2);
+  });
+
+  it('Should have property "total" on initialization.', () => {
+    const carrinho = new Carrinho();
+
+    expect(carrinho).toHaveProperty('total');
+  });
+
+  it('Should throw exception when trying finish the purchase with empty cart.', () => {
+    const wrappedFunction = () => {
+      const carrinho = new Carrinho();
+      carrinho.finalizaCompra();
+    };
+
+    expect(wrappedFunction).toThrow('Carrinho de compras vazio');
+  });
+
+  it('Should add freight.', () => {
+    const carrinho = new Carrinho();
+    carrinho.adicionaFrete(110);
+
+    expect(carrinho.frete).toBe(110);
+  });
+
+  it('Should finish the purchase.', () => {
+    const item1 = new Item('Banana', 2, 5);
+    const item2 = new Item('Mel', 3, 10);
+
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item1);
+    carrinho.adiciona(item2);
+    carrinho.adicionaFrete(10);
+
+    expect(carrinho.finalizaCompra()).toStrictEqual({
+      subtotal: 40,
+      frete: 10,
+      total: 50,
     });
-
-    it('Should have items.', () => {
-        const item1 = new Item('Beterraba', 2.5, 10);
-        const item2 = new Item('Beterraba', 3, 5); 
-
-        const carrinho = new Carrinho();
-        carrinho.adiciona(item1);
-        carrinho.adiciona(item2);
-
-
-        expect(typeof carrinho.itens).toBe('object');
-        expect(carrinho.itens[0]).toEqual(item1);
-        expect(carrinho.itens[1]).toEqual(item2);
-
-        expect(carrinho.itens).toContain(item1);
-        expect(carrinho.itens).toContain(item2);
-    });
-
-    it('Should have property "total" on initialization.', () => {
-        const carrinho = new Carrinho();
-
-        expect(carrinho).toHaveProperty("total");
-    })
-
-    
-    it('Should throw exception when trying finish the purchase with empty cart.', () => {
-        const wrappedFunction = () => {
-            const carrinho = new Carrinho();
-            carrinho.finalizaCompra(); 
-        };
-
-        expect(wrappedFunction).toThrow('Carrinho de compras vazio');
-    });
-})
+  });
+});
