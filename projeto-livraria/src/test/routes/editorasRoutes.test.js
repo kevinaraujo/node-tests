f3317aa82a1ea75852f03dbb90334995f909e55e
Kevin Araújo <kfa_34@hotmail.com>
2024-02-06 09:53:49 -0300

    test: Added new integration test with get, delete and post on /editoras routes.

--- projeto-livraria/src/test/routes/editorasRoutes.test.js
+++ projeto-livraria/src/test/routes/editorasRoutes.test.js
@@ -1,4 +1,5 @@
-import { afterAll, afterEach, beforeEach } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
+import request from 'supertest';
 import app from '../../app.js';
 
 let server;
@@ -10,4 +11,47 @@ beforeEach(() => {
 
 afterEach(() => {
   server.close();
+});
+
+describe('GEt in /editoras.', () => {
+  it('Should return a editoras list.', async () => {
+    const res = await request(app)
+      .get('/editoras')
+      .set('accept', 'application/json')
+      .expect('content-type', /json/)
+      .expect(200);
+
+    expect(res.body[0].email).toEqual('e@e.com');
+  });
+});
+
+let idResponse;
+describe('POST in /editoras.', () => {
+  it('Should add a new editora', async () => {
+    const res = await request(app)
+      .post('/editoras')
+      .send({
+        nome: 'CDC',
+        cidade: 'califa',
+        email: 's@s.com',
+      })
+      .expect(201);
+    idResponse = res.body.content.id;
+  });
+});
+
+describe('DELETE in /editoras', () => {
+  it('Should delete the recorded added', async () => {
+    await request(app)
+      .delete(`/editoras/${idResponse}`)
+      .expect(200);
+  });
+});
+
+describe('GET in /editoras/id', () => {
+  it('Should return the especific resource by id', async () => {
+    await request(app)
+      .get(`/editoras/${idResponse}`)
+      .expect(200);
+  });
 });
